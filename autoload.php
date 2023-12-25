85fdb9d5d5ed4180967802f73f34d34c5f0948de
Abdelrahman Mostafa <abdelrahmandeveloper123@gmail.com>
2023-12-25 06:19:27 +0200

    Added AdmintLTE3 to twig template and env file loader

--- autoload.php
+++ autoload.php
@@ -1,8 +1,11 @@
 <?php
-
+// Include vendor autoload
 include_once './vendor/autoload.php';
+
+// Load .env file from the root directory
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 // Create a Twig loader and environment
 $loader = new \Twig\Loader\FilesystemLoader('resources/template');
 $twig = new \Twig\Environment($loader);
-
--- logs.php
+++ logs.php
@@ -2,9 +2,7 @@
 require_once "./autoload.php";
 
 $template = $twig->load('index.twig');
-
-
 echo $template->render([
-    'title' => 'Dashboard'. $_ENV['ROOT_PASSWORD'],
+    'title' => 'Logs',
     'content' => 'This is a simple example of using Twig in PHP.'
 ]);
