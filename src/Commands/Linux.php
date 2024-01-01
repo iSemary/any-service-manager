22aa85d84cc393abadd945d99f2e42a7a4b6c011
Abdelrahman Samir <abdelrahmandeveloper123@gmail.com>
2024-01-02 00:25:38 +0200

    remove unused files

--- src/Commands/Linux.php
+++ src/Commands/Linux.php
@@ -3,10 +3,13 @@
 namespace Isemary\AnyServiceManager\Commands;
 
 interface Linux {
+    const ROOT_COMMAND = "sudo";
     const SYSTEMCTL_STATUS = "systemctl status";
     const INSTALL_COMMAND = "apt install";
     const UNINSTALL_COMMAND = "apt remove";
     const PURGE_COMMAND = "apt purge";
     const CHECK_VERSION_COMMAND = "--version";
     const FIND_COMMAND = "which";
+    const POWER_OFF = "/sbin/poweroff";
+    const RESTART = "sudo /sbin/reboot";
 }
